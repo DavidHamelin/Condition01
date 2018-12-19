f08eae802fea4d8b3b596d1166aa7995440deccc
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 10:23:21 +0100

    ex 01 conditions

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rainy = true;
+            if (rainy == true) Console.WriteLine("Bonjour, il ne pleut pas aujourd'hui, bonne journée !");
+            else Console.WriteLine("Bonjour, il pleut aujourd'hui, vous devriez prendre votre parapluie !");  
+        }
+    }
+}
